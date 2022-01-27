ce7529590479a8002a398e1de096d4ee451dc0a4
sai teja <saitejathota134@msitprogram.net>
2022-01-27 13:49:53 +0530

    GenerateTextFromUnigram

--- language.py
+++ language.py
@@ -207,7 +207,12 @@ Returns: str
 '''
 from random import choices
 def generateTextFromUnigrams(count, words, probs):
-    return
+    Text=""
+    for i in range(count):
+        sentence=choices(words, weights=probs)
+        Text=Text+" "+sentence[0]
+    return Text
+    
 
 
 '''
@@ -382,7 +387,8 @@ if __name__ == "__main__":
     test.runWeek2()
     #test.testBuildUniformProbs()
     #test.testBuildBigramProbs()
-    test.testGetTopWords()
+    #test.testGetTopWords()
+    test.testGenerateTextFromUnigrams()
 
 
     ## Uncomment these for Week 3 ##
